5a09982cf7a54f21126a5081a34a7c818445eeb8
John Schibelli <jschibelli@gmail.com>
2024-02-22 17:45:21 -0500

    debugging the plan section for the maintanence category

--- components/Plans/plans.tsx
+++ components/Plans/plans.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Container from "../container";
 import style from "./plans.module.scss";
-import Maintanence from "./Maintanence/maintanence";
 import Development from "./Development/development";
+import MaintenancePlans from "./Maintanence/maintanence";
 
 
 const Plans: React.FC = () => {
@@ -10,7 +10,7 @@ const Plans: React.FC = () => {
     <Container>
       <div className="max-w-7xl mx-auto">
         <h2 className="display-5 fw-normal text-center">Maintanence Plans</h2>
-        <Maintanence />
+        <MaintenancePlans
         <h2 className="display-5 fw-normal text-center">Development</h2>
         <Development />
       </div>
