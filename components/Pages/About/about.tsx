b364a535d116ec1f555be85e911ce5bf60c35e23
John Schibelli <jschibelli@gmail.com>
2024-02-24 12:30:47 -0500

    Added base page styles

--- components/Pages/About/about.tsx
+++ components/Pages/About/about.tsx
@@ -13,14 +13,27 @@ const AboutPage = () => {
   }, []);
 
   return (
-    <div>
-      {/* Check if about exists before attempting to access its properties */}
-      {about && (
-        <div key={about.id}>
-          <h1>{about.title.rendered}</h1>
-          <div dangerouslySetInnerHTML={{ __html: about.content.rendered }} />
+    <div className="resume-page">
+      <div className="max-w-5xl mx-auto">
+        <div className="px-4 py-5 my-5 text-center">
+          <h1 className="display-5 fw-bold h1">Welcome to my universe!</h1>
+          <div className="col-lg-6 mx-auto">
+            <p className="lead mb-4">
+              Hey there! I’m John, a passionate full stack developer with a
+              mission to blend creativity with functionality in every line of
+              code. Welcome to my digital domain, where innovation meets
+              execution, and pixels transform into possibilities.
+            </p>
+          </div>
         </div>
-      )}
+        {/* Check if about exists before attempting to access its properties */}
+        {about && (
+          <div key={about.id}>
+            <h1>{about.title.rendered}</h1>
+            <div dangerouslySetInnerHTML={{ __html: about.content.rendered }} />
+            </div>
+          )}
+        </div>
     </div>
   );
 };
