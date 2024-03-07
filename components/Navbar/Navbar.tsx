c41e28025e6aad6fe40121e70dbd3fba9f2205fb
John Schibelli <jschibelli@gmail.com>
2024-03-07 11:58:04 -0500

    Fix casing of import statement in Navbar component

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import styles from "./Navbar.module.scss";
+import styles from "./navbar.module.scss";
 import {
   Navbar,
   NavbarBrand,
