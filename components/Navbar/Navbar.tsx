d8cef5af1f14431e4337a4af9dd3b570a335ee65
John Schibelli <jschibelli@gmail.com>
2024-03-07 11:48:09 -0500

    Refactor Navbar component and add styles

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
+import styles from "./Navbar.module.scss";
 import {
   Navbar,
   NavbarBrand,
@@ -23,81 +24,88 @@ export default function NavbarComponent() {
   ];
 
   return (
-    <Navbar isBordered isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen}>
-      <NavbarContent className="sm:hidden" justify="start">
-        <NavbarMenuToggle
-          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-        />
-      </NavbarContent>
-      <NavbarContent className="sm:hidden pr-3" justify="center">
-        <NavbarBrand>
-          <p className="font-bold text-inherit">SCHIBELLI</p>
-        </NavbarBrand>
-      </NavbarContent>
+    <div className={styles.navbar_nav}>
+      <Navbar
+        isBordered
+        isMenuOpen={isMenuOpen}
+        onMenuOpenChange={setIsMenuOpen}
+      >
+        <NavbarContent className="sm:hidden" justify="start">
+          <NavbarMenuToggle
+            aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          />
+        </NavbarContent>
+        <NavbarContent className="sm:hidden pr-3" justify="center">
+          <NavbarBrand>
+            <p className="font-bold text-inherit">SCHIBELLI</p>
+          </NavbarBrand>
+        </NavbarContent>
 
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarBrand>
-          <p className="font-bold text-inherit">
-            <Link href="/" aria-current="page">
-              SCHIBELLI.com
+        <NavbarContent
+          className="hidden sm:flex gap-4 text-white"
+          justify="center"
+        >
+          <NavbarBrand>
+            <p className="font-bold text-inherit">
+              <Link href="/" aria-current="page">
+                SCHIBELLI.com
+              </Link>
+            </p>
+          </NavbarBrand>
+          <NavbarItem>
+            <Link color="foreground" href="/about">
+              About
             </Link>
-          </p>
-        </NavbarBrand>
-        <NavbarItem>
-          <Link color="foreground" href="/about">
-            About
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="/resume">
-            Resume
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link href="/hire" aria-current="page" color="warning">
-            Hire Me
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="/">
-            Blog
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="/contact">
-            Contact
-          </Link>
-        </NavbarItem>
-      </NavbarContent>
-      <div >
-      <NavbarMenu>
-        <NavbarItem>
-          <Link color="foreground" href="/about">
-            About
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="/resume">
-            Resume
-          </Link>
-        </NavbarItem>
-        <NavbarItem isActive>
-          <Link href="/hire" aria-current="page" color="warning">
-            Hire Me
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="/">
-            Blog
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="/contact">
-            Contact
-          </Link>
-        </NavbarItem>
+          </NavbarItem>
+          <NavbarItem>
+            <Link color="foreground" href="/resume">
+              Resume
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link href="/hire" color="warning">
+              Hire Me
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link color="foreground" href="/">
+              Blog
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link color="foreground" href="/contact">
+              Contact
+            </Link>
+          </NavbarItem>
+        </NavbarContent>
+        <NavbarMenu>
+          <NavbarItem>
+            <Link className="text-white" href="/about">
+              About
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link className="text-white" href="/resume">
+              Resume
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link className="text-white" href="/hire" aria-current="page">
+              Hire Me
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link className="text-white" href="/">
+              Blog
+            </Link>
+          </NavbarItem>
+          <NavbarItem>
+            <Link className="text-white" href="/contact">
+              Contact
+            </Link>
+          </NavbarItem>
         </NavbarMenu>
-        </div>
-    </Navbar>
+      </Navbar>
+    </div>
   );
 }
