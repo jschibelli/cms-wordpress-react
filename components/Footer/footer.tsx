b5081ffbedf4764ea670687d88336bf302004845
John Schibelli <jschibelli@gmail.com>
2024-03-07 18:40:17 -0500

    Add react-icons to Footer component

--- components/Footer/footer.tsx
+++ components/Footer/footer.tsx
@@ -1,28 +1,50 @@
+import React from "react";
 import Container from "../container";
-import { EXAMPLE_PATH } from "../../lib/constants";
+import { FaGithub, FaLinkedin, FaBriefcase } from "react-icons/fa"; // Ensure you have installed react-icons
+import style from "./footer.module.scss";
 
 export default function Footer() {
   return (
-    <footer className="bg-accent-1 border-t border-accent-2">
+    <footer className={style.site_footer}>
       <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
-          </h3>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            <a
-              href="https://nextjs.org/docs/basic-features/pages"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-            >
-              Read Documentation
-            </a>
-            <a
-              href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-              className="mx-3 font-bold hover:underline"
-            >
-              View on GitHub
-            </a>
+        <div
+          className={`flex flex-col lg:flex-row items-center ${style.footer_container}`}
+        >
+          <div
+            className={`${style.footer_left} text-xl lg:text-5xl font-bold tracking-tighter leading-tight text-center mb-10 lg:mb-0 lg:pr-4 lg:w-1/2`}
+          >
+            <p></p>
+            {/* Social links and icons */}
+            <div className={style.social_links}>
+              <a
+                href="https://github.com/yourusername"
+                className={style.social_link}
+              >
+                <FaGithub />
+              </a>
+              <a
+                href="https://linkedin.com/in/yourusername"
+                className={style.social_link}
+              >
+                <FaLinkedin />
+              </a>
+              <a
+                href="https://upwork.com/freelancers/yourusername"
+                className={style.social_link}
+              >
+                <FaBriefcase />
+              </a>
+            </div>
           </div>
+          <div
+            className={`${style.footer_right} flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2`}
+          >
+            {/* Other links or content */}
+          </div>
+        </div>
+        {/* Copyright notice */}
+        <div className={style.copywrite}>
+          <p>© 2024 schibelli.com</p>
         </div>
       </Container>
     </footer>
