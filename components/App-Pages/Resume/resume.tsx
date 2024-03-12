094c328aca53c9565d677b71305b5c0a1003408a
John Schibelli <jschibelli@gmail.com>
2024-03-11 23:00:31 -0400

    Update resume page layout and add target and rel attributes to resume download link

--- components/App-Pages/Resume/resume.tsx
+++ components/App-Pages/Resume/resume.tsx
@@ -8,13 +8,15 @@ const ResumePage = () => {
       <div className="max-w-5xl mx-auto">
         <div className="px-4 py-5 my-5 text-center">
           <h1 className="display-5 fw-bold h1">John Schibelli</h1>
-          <div className="col-lg-6 mx-auto">
+          <div className="col-lg-6 mx-auto w-1/4">
             <p className="lead mb-4">Full-Stack AI Engineer</p>
-            <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+            <div className="d-grid gap-2 d-sm-flex justify-content-sm-center ">
               <div id="choose" className="w-auto mt-10 px-6">
                 <a
                   href="https://intrawebtech.com/wp-content/uploads/2024/03/John-Schibelli_Resume-March-2024.pdf"
-                  className="w-full block bg-gray-900 font-medium text-xl py-4 rounded-xl hover:shadow-lg transition duration-200 ease-in-out hover:bg-indigo-600 hover:text-white"
+                  className=" w-full block bg-gray-900 font-medium text-xl py-4 rounded-xl hover:shadow-lg transition duration-200 ease-in-out hover:bg-indigo-600 hover:text-white"
+                  target="_blank"
+                  rel="noreferrer"
                 >
                   Download Resume
                 </a>
