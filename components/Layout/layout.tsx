45dfd181a58f70df3911ef16f3601a988bcdd80e
John Schibelli <jschibelli@gmail.com>
2024-03-12 20:45:42 -0400

    Add @vercel/speed-insights package

--- components/Layout/layout.tsx
+++ components/Layout/layout.tsx
@@ -3,6 +3,7 @@ import Alert from "../Alert/alert";
 import Footer from "../Footer/footer";
 import Meta from "../Meta/meta";
 import NavbarComponant from "../Navbar/Navbar";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Layout({ preview, children }) {
   return (
