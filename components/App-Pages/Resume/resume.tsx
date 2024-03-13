8e3e0909fc9ff8168ef1b29eabe38203ae54317f
John Schibelli <jschibelli@gmail.com>
2024-03-12 20:42:48 -0400

    test

--- components/App-Pages/Resume/resume.tsx
+++ components/App-Pages/Resume/resume.tsx
@@ -9,7 +9,7 @@ const ResumePage = () => {
         <div className="px-4 py-5 my-5 text-center">
           <h1 className="display-5 fw-bold h1">John Schibelli</h1>
           <div className="col-lg-6 mx-auto max-w-md">
-            <p className="lead mb-4">Full-Stack AI Engineer</p>
+            <p className="lead mb-4">Full-Stack AI Engineerr</p>
             <div className="d-grid gap-2 d-sm-flex justify-content-sm-center  ">
               <div id="choose" className=" mt-10 px-6">
                 <a
