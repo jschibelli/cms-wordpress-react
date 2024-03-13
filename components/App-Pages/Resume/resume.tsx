74feaf71010ef51781cf97331cdbc8778bc25dde
John Schibelli <jschibelli@gmail.com>
2024-03-12 20:30:13 -0400

    Update resume page layout and styles

--- components/App-Pages/Resume/resume.tsx
+++ components/App-Pages/Resume/resume.tsx
@@ -5,13 +5,13 @@ import Employers from "../../Employer/employer";
 const ResumePage = () => {
   return (
     <div className="resume-page">
-      <div className="max-w-5xl mx-auto">
+      <div className="max-w-5xl mx-auto ">
         <div className="px-4 py-5 my-5 text-center">
           <h1 className="display-5 fw-bold h1">John Schibelli</h1>
-          <div className="col-lg-6 mx-auto w-1/4">
+          <div className="col-lg-6 mx-auto max-w-md">
             <p className="lead mb-4">Full-Stack AI Engineer</p>
-            <div className="d-grid gap-2 d-sm-flex justify-content-sm-center ">
-              <div id="choose" className="w-auto mt-10 px-6">
+            <div className="d-grid gap-2 d-sm-flex justify-content-sm-center  ">
+              <div id="choose" className=" mt-10 px-6 max-w-md ">
                 <a
                   href="../../../docs/John-Schibelli_Resume-March-2024.pdf"
                   className=" w-full block bg-gray-900 font-medium text-xl py-4 rounded-xl hover:shadow-lg transition duration-200 ease-in-out hover:bg-indigo-600 hover:text-white"
