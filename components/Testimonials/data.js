103dc3c2ef5b57e311b4f0cfeb95f25069bf7efd
John Schibelli <jschibelli@gmail.com>
2024-03-24 13:45:12 -0400

    added a data schema for the testimonals

---
+++ components/Testimonials/data.js
@@ -0,0 +1,35 @@
+const testimonial = [
+  {
+    userId: 1,
+    content:
+      "John Schibelli is one of the most stand up, motivated people I know. His superior business sense, artistic ability, troubleshooting andflexible adaptabilty is only equal to his kindness, patience and leadership qualities. John's fierce loyalty to his colleagues and morals makes him someone that you want to work with. You would be very lucky to have John on your team.",
+    name: "Jason D'Amico",
+    description: "Senior Web Developer",
+    avatarProps: {
+      src: "https://media.licdn.com/dms/image/D4E35AQG7AZhNz0MtUw/profile-framedphoto-shrink_800_800/0/1707920449142?e=1711821600&v=beta&t=zD3AGdYgi7xclh3IIV31GxAEeQJwzA4_XFtm3jShZQM",
+    },
+  },
+  {
+    userId: 2,
+    content:
+      "John was a dedicated hard-working manager. Who was able to resolve the initial issues found in our department left by past management. He worked long and hard to develop the department into a strong group of instructors who were recognized as some of the best in the company. This included balancing budgets, instructor development, and helping to enhance both customer and employee satisfaction. John is a team player; leader and I would recommend him without question for a management role.",
+    name: "Joshua Trujillo",
+    description: "Marketing Manager",
+    avatarProps: {
+      src: "https://media.licdn.com/dms/image/D4E03AQH6CC6TaovvZw/profile-displayphoto-shrink_200_200/0/1682098095748?e=1716422400&v=beta&t=oLvJzze9nv8rume_EqGRXYajsEM42HE6LgDbRrtkl9g",
+    },
+  },
+  {
+    userId: 3,
+    content:
+      "John was instrumental in helping take a new program and turning it into one of the most respected and profitable programs at Anthem Institute. John took the time to hire a team of instructors, including myself, who's skills and personalities complimented each other. This proved to be an excellent strategy as it meant the team was stronger and worked much better together. John always promoted our development and worked hard to make sure the resources were available for us to do so. He is very well organized, creative, and has excellent leadership skills. It was a pleasure working with him.",
+    name: "Walid Elshahed",
+    description: "Creative Director | Frontend Developer | UX Wizard",
+    avatarProps: {
+      src: "https://media.licdn.com/dms/image/D4D03AQG717x-p356AA/profile-displayphoto-shrink_200_200/0/1703867939567?e=1716422400&v=beta&t=ZBMm3kESbh4rkZvoEekkwGbzzqSQVInop-NFB1SmBVc",
+    },
+  },
+  // Add more objects following the same schema as needed
+];
+
+export default testimonial;
