d3fecad2d4a721592b1aeee0b88c09a8f34779a6
John Schibelli <jschibelli@gmail.com>
2024-03-31 08:56:56 -0400

    Refactor testimonial carousel component

--- components/Carousels/Testimonials/Testimonials.tsx
+++ components/Carousels/Testimonials/Testimonials.tsx
@@ -1,12 +1,18 @@
-import React from "react";
-import Marquee from "react-fast-marquee";
-import Carousel from "react-bootstrap/Carousel";
-import Image from "next/image";
+import React, { useState, useEffect } from "react";
 import Styles from "./testimonials.module.scss";
 import { User } from "@nextui-org/user";
 import testimonial from "./data-testimonies";
 
 const Testimonials = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveIndex((current) => (current + 1) % testimonial.length);
+    }, 7000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="px-4 py-5 my-5 text-center">
       <h1 className="display-5 fw-bold h1">Testimonials</h1>
@@ -19,25 +25,40 @@ const Testimonials = () => {
           outcomes forged through our joint endeavors.
         </p>
       </div>
-      <div>
-      {testimonial.map((item) => (
-        <div
-          key={item.userId}
-          className={`${Styles.testimonial_item} max-w-md`}
-        >
-          <div className={`${Styles.testimonial_content}`}>{item.content}</div>
-          <div className={`${Styles.testimonial_meta}`}>
-            <User
-              name={item.name}
-              description={item.description}
-              avatarProps={{
-                src: item.avatarProps.src,
-              }}
-            />
+      <div className={Styles.testimonialCarouselContainer}>
+        {testimonial.map((item, index) => (
+          <div
+            key={item.userId}
+            className={`${Styles.testimonialItem} ${
+              index === activeIndex ? Styles.active : ""
+            }`}
+          >
+            {index === activeIndex && (
+              <>
+                <div className={Styles.testimonial_content}>{item.content}</div>
+                <div className={Styles.testimonial_meta}>
+                  <User
+                    name={item.name}
+                    description={item.description}
+                    avatarProps={{ src: item.avatarProps.src }}
+                  />
+                </div>
+              </>
+            )}
           </div>
+        ))}
+        <div className={Styles.carouselIndicators}>
+          {testimonial.map((_, index) => (
+            <span
+              key={index}
+              className={`${Styles.carouselIndicator} ${
+                index === activeIndex ? Styles.activeIndicator : ""
+              }`}
+              onClick={() => setActiveIndex(index)}
+            ></span>
+          ))}
         </div>
-      ))}
-    </div>
+      </div>
     </div>
   );
 };
--- components/Carousels/Testimonials/data-testimonies.js
+++ components/Carousels/Testimonials/data-testimonies.js
@@ -24,7 +24,7 @@ const testimonial_Data = [
     content:
       "John was instrumental in helping take a new program and turning it into one of the most respected and profitable programs at Anthem Institute. John took the time to hire a team of instructors, including myself, who's skills and personalities complimented each other. This proved to be an excellent strategy as it meant the team was stronger and worked much better together. John always promoted our development and worked hard to make sure the resources were available for us to do so. He is very well organized, creative, and has excellent leadership skills. It was a pleasure working with him.",
     name: "Walid Elshahed",
-    description: "Creative Director | Frontend Developer | UX Wizard",
+    description: "Frontend Developer",
     avatarProps: {
       src: "https://media.licdn.com/dms/image/D4D03AQG717x-p356AA/profile-displayphoto-shrink_200_200/0/1703867939567?e=1716422400&v=beta&t=ZBMm3kESbh4rkZvoEekkwGbzzqSQVInop-NFB1SmBVc",
     },
