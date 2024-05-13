5cf18741bfaf36ab8b8d4e402906c6a2032b5f66
John Schibelli <jschibelli@gmail.com>
2024-05-13 15:25:41 -0400

    refactor: Add pagination component to home page

--- components/Pagination/Pagination.tsx
+++ components/Pagination/Pagination.tsx
@@ -1,11 +1,12 @@
+// Pagination.js or wherever your Pagination component is defined
 const Pagination = ({ currentPage, totalPages, onPageChange }) => {
   return (
     <div className="pagination">
       {[...Array(totalPages)].map((_, index) => (
         <button
           key={index}
-          onClick={() => onPageChange(index + 1)}
           disabled={currentPage === index + 1}
+          onClick={() => onPageChange(index + 1)} // Uses onPageChange prop to handle clicks
         >
           {index + 1}
         </button>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import { GetStaticProps } from "next";
-import { useState } from "react";
 import Container from "../components/container";
 import MoreStories from "../components/More-Stories/more-stories";
 import HeroPost from "../components/Hero-Post/hero-post";
@@ -8,22 +7,10 @@ import Intro from "../components/Intro/intro";
 import Layout from "../components/Layout/layout";
 import { getAllPostsForHome } from "../lib/api";
 import { CMS_NAME } from "../lib/constants";
-import Pagination from "../components/Pagination/Pagination"; // Assuming you have or will create a simple Pagination component
 
-export default function Index({ allPosts, preview }) {
-  const edges = allPosts?.edges || []; // Provide a default empty array if edges are undefined
-  const [page, setPage] = useState(1);
-  const limit = 10; // Adjust based on your preference
-
+export default function Index({ allPosts: { edges }, preview }) {
   const heroPost = edges[0]?.node;
   const morePosts = edges.slice(1);
-
-  // Function to handle page change
-  const handlePageChange = (newPage) => {
-    setPage(newPage);
-    // Here you should also refetch the posts based on the new page
-    // This could be achieved by modifying the getStaticProps to server-side props if real-time data is needed
-  };
 
   return (
     <Layout preview={preview}>
@@ -43,19 +30,16 @@ export default function Index({ allPosts, preview }) {
           />
         )}
         {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        <Pagination currentPage={page} handlePageChange={handlePageChange} />
       </Container>
     </Layout>
   );
 }
 
-export const getStaticProps = async () => {
-  const allPosts = await getAllPostsForHome(false, 1, 7); // Ensure this function call is correct
+export const getStaticProps: GetStaticProps = async ({ preview = false }) => {
+  const allPosts = await getAllPostsForHome(preview);
+
   return {
-    props: {
-      allPosts: allPosts || {}, // Provide a default empty object if allPosts is undefined
-      preview: false,
-    },
-    revalidate: 8,
+    props: { allPosts, preview },
+    revalidate: 10,
   };
 };
