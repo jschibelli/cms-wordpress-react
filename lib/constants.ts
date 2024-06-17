90c4209f3a670f90cb62dfa802f168ad671aff6f
John Schibelli <jschibelli@gmail.com>
2024-06-16 20:22:50 -0400

    refactor: Update API key and OG image URL

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,4 +2,4 @@ export const EXAMPLE_PATH = "cms-wordpress";
 export const CMS_NAME = "WordPress";
 export const CMS_URL = "https://wordpress.org";
 export const HOME_OG_IMAGE_URL =
-  "https://intrawebtech.com/wp-content/uploads/2024/06/openai_battels_misinformation.webp";
+  "https://intrawebtech.com/wp-content/uploads/2024/06/Leveraging-AI-in-Development-Workflows.webp";
